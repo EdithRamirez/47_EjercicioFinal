9c10655e54c87a69c01d421339b864047d1f5476
EdithRamirez <ediths.ramirez07@gmail.com>
2016-12-06 13:37:55 -0600

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -4,23 +4,24 @@ $(document).ready(function() {
 	//ocultar menu
 	$('.js-menu').hide();
 
-
+	//boton js-show-recipe elimina la clase make al dar click
 	$('.js-show-recipe').on('click', function() {
 		console.log('Quite clase make');
 		$('.page.recipe').removeClass('make');
+
+		/*Si se hace click en js-show-recipe agrega la clase active y quitasela a js-show-make.*/
+		$('.js-show-recipe').addClass('active');
+		$('.js-show-make').removeClass('active');
+
 	});
 
+	//boton js-show-make agrega la clase make al dar click
 	$('.js-show-make').on('click', function() {
 		console.log('Agregue clase make');
 		$('.page.recipe').addClass('make');
+		
+		/*Si se hace click en js-show-make agregale la clase active y quitasela a js-show-recipe.*/
+		$('.js-show-make').addClass('active');
+		$('.js-show-recipe').removeClass('active');
 	});
-
-
 });
-
-/*Asigna funciones que se ejecuten cuando el usuario de click en las pestañas 
-(2 elementos a con clase js-show-recipe y js-show-make, estos 2 estan dentro de un DIV con clase btns-group).*/
-
-
-/*	Identifica el DIV que contiene la mayoria del codigo, tiene las clases page y recipe. Agregale la clase make cuando se 
-haga click en el elemento js-show-make. Y si el usuario hace click en el elemento js-show-recipe entonces quitale la clase make al DIV.*/
